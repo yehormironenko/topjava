68e128e1cea88f90df7a4dc10c2a8aa8ee7ea498
Yahor Mironenka <wazap_by@mail.ru>
2018-10-09 03:58:50 +0200

    added id

--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -3,6 +3,8 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealWithExceed {
+    private final Integer id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,7 +13,8 @@ public class MealWithExceed {
 
     private final boolean exceed;
 
-    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public MealWithExceed(Integer id, LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -20,6 +23,10 @@ public class MealWithExceed {
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getDescription() {
