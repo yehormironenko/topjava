0b976c63c77288808c1618ba4922d03532f9d421
Yahor Mironenka <wazap_by@mail.ru>
2018-10-16 15:55:52 +0200

    isBetween with generics

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -7,7 +7,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+
+    public static <T extends Comparable<? super T>> boolean isBeetween(T lt, T startTime, T endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
 
