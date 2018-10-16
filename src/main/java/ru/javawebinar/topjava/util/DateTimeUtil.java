a259cbd6f1f2bd71a2d7b91046bdf149bf09025b
Yahor Mironenka <wazap_by@mail.ru>
2018-10-16 15:58:22 +0200

    renamed method isBetween

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -8,7 +8,7 @@ public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
 
-    public static <T extends Comparable<? super T>> boolean isBeetween(T lt, T startTime, T endTime) {
+    public static <T extends Comparable<? super T>> boolean isBetween(T lt, T startTime, T endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
 
