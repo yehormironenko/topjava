162bd8f2c9e6addf1f2f71492d7220bb49939292
Yahor Mironenka <wazap_by@mail.ru>
2019-03-06 21:32:53 +0100

    4 fix get and logs

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -46,7 +46,7 @@ public class JpaMealRepositoryImpl implements MealRepository {
     @Override
     public Meal get(int id, int userId) {
         Meal meal = em.find(Meal.class, id);
-        if (meal.getUser().getId() == userId) {
+        if (meal.getUser().getId() == userId && meal!=null) {
             return meal;
         } else return null;
     }
--- src/test/java/ru/javawebinar/topjava/util/MyJUnitStopWatch.java
+++ src/test/java/ru/javawebinar/topjava/util/MyJUnitStopWatch.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.util;
 
-import org.junit.AssumptionViolatedException;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
 
@@ -11,39 +10,18 @@ public class MyJUnitStopWatch extends Stopwatch {
 
     private static final Logger log = Logger.getLogger(String.valueOf(MyJUnitStopWatch.class));
 
-    private static StringBuilder sb = new StringBuilder().append("\nFINAL RESULTS");
-
-    private static void logInfo(Description description, String status, long nanos) {
-        String testName = description.getMethodName();
-        log.info(String.format("Test %s %s, spent %d microseconds",
-                testName, status, TimeUnit.NANOSECONDS.toMicros(nanos)));
-        sb.append(String.format("\nTest %s %s, spent %d microseconds",
-                testName, status, TimeUnit.NANOSECONDS.toMicros(nanos)));
-    }
+    private static StringBuilder sb = new StringBuilder().append("\nFINAL RESULTS\n");
 
     public static void finalResult() {
         log.info(String.valueOf(sb));
     }
 
     @Override
-    protected void succeeded(long nanos, Description description) {
-        logInfo(description, "succeeded", nanos);
-    }
-
-    @Override
-    protected void failed(long nanos, Throwable e, Description description) {
-        logInfo(description, "failed", nanos);
-    }
-
-    @Override
-    protected void skipped(long nanos, AssumptionViolatedException e, Description description) {
-        logInfo(description, "skipped", nanos);
-    }
-
-    @Override
     protected void finished(long nanos, Description description) {
-        logInfo(description, "finished", nanos);
+        String testName = description.getMethodName();
+        String res = String.format("Test %s finished, spent %d milliseconds\n",
+                testName, TimeUnit.NANOSECONDS.toMillis(nanos));
+        sb.append(res);
+        log.info(res);
     }
-
-
 }
