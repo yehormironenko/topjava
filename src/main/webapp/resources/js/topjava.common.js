3f1d0cc58d8c6accf31094788b20e96aca95a7c1
Yahor Mironenka <wazap_by@mail.ru>
2019-04-05 19:28:56 +0200

    8 08 refactor js

---
+++ src/main/webapp/resources/js/topjava.common.js
@@ -0,0 +1,42 @@
+let context, form;
+
+function makeEditable(ctx) {
+    context = ctx;
+    form = $('#detailsForm');
+    $(".delete").click(function () {
+        if (confirm('Are you sure?')) {
+            deleteRow($(this).attr("id"));
+        }
+    });
+}
+
+function add() {
+    form.find(":input").val("");
+    $("#editRow").modal();
+}
+
+function deleteRow(id) {
+    $.ajax({
+        url: context.ajaxUrl + id,
+        type: "DELETE"
+    }).done(function () {
+        updateTable();
+    });
+}
+
+function updateTable() {
+    $.get(context.ajaxUrl, function (data) {
+        context.datatableApi.clear().rows.add(data).draw();
+    });
+}
+
+function save() {
+    $.ajax({
+        type: "POST",
+        url: context.ajaxUrl,
+        data: form.serialize()
+    }).done(function () {
+        $("#editRow").modal("hide");
+        updateTable();
+    });
+}
